f9f761c1edd75b11ffa310913202ff1fa73bd49b
Daria Churkina <d_churkina@inbox.ru>
2016-05-29 14:49:53 +0300

    Создан класс Транспондер

---
+++ sandbox/src/main/java/Transponder.java
@@ -0,0 +1,13 @@
+public class Transponder {
+
+    double priceTruck;
+    double priceBus;
+    double priceCar;
+
+    public Transponder(double priceTruck, double priceBus, double priceCar) {
+
+        this.priceBus = priceBus;
+        this.priceTruck = priceTruck;
+        this.priceCar = priceCar;
+    }
+}
