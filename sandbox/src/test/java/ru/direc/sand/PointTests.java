0edf204aa2d90aa26971b5239a22b3e1ad9a05c4
Daria Churkina <d_churkina@inbox.ru>
2016-06-01 21:59:25 +0300

    Создан новый тест для класса Point
    Вариант теста с функцией distanceTo

---
+++ sandbox/src/test/java/ru/direc/sand/PointTests.java
@@ -0,0 +1,16 @@
+package ru.direc.sand;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PointTests {
+
+    @Test
+    public void testPoint() {
+        Point p1 = new Point(1, 3);
+        Point p2 = new Point(7, 3);
+        Assert.assertEquals(p1.distanceTo(p2), 6.0);
+    }
+
+
+}
