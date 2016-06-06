87ab3cec6e46a7fc6d629ecda5dd460d9a435793
DariaChurkina <d_churkina@inbox.ru>
2016-06-06 21:29:20 +0300

    Вспомогательные методы из классов ContactCreationTest, ContactDeletionTest вынесена в базовый класс TestBase.

--- addressbook_web_tests/src/test/java/ru/stqa/pft/addressbook/TestBase.java
+++ addressbook_web_tests/src/test/java/ru/stqa/pft/addressbook/TestBase.java
@@ -81,4 +81,43 @@ public class TestBase {
     protected void selectGroup() {
         wd.findElement(By.name("selected[]")).click();
     }
+
+    protected void goToHomePage() {
+        wd.findElement(By.linkText("home")).click();
+    }
+
+    protected void submitNewContact() {
+        wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
+    }
+
+    protected void fillContactData(ContactData contactData) {
+        wd.findElement(By.name("firstname")).click();
+        wd.findElement(By.name("firstname")).clear();
+        wd.findElement(By.name("firstname")).sendKeys(contactData.getFirstname());
+        wd.findElement(By.name("lastname")).click();
+        wd.findElement(By.name("lastname")).clear();
+        wd.findElement(By.name("lastname")).sendKeys(contactData.getLastname());
+        wd.findElement(By.name("home")).click();
+        wd.findElement(By.name("home")).clear();
+        wd.findElement(By.name("home")).sendKeys(contactData.getPhone());
+        wd.findElement(By.name("email")).click();
+        wd.findElement(By.name("email")).clear();
+        wd.findElement(By.name("email")).sendKeys(contactData.getEmail());
+    }
+
+    protected void goToAddNewPage() {
+        wd.findElement(By.linkText("add new")).click();
+    }
+
+    protected void getAccept() {
+        wd.switchTo().alert().accept();
+    }
+
+    protected void deleteSelectedContact() {
+        wd.findElement(By.xpath("//div[@id='content']/form[2]/div[2]/input")).click();
+    }
+
+    protected void selectContact() {
+        wd.findElement(By.name("selected[]")).click();
+    }
 }
