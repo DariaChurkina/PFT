d3da727ee688a674328eb8694f7ace0f06712af9
DariaChurkina <d_churkina@inbox.ru>
2016-06-16 00:55:03 +0300

    NavigationHelper оптимизирован для реализации проверки места нахождения в приложении.

--- addressbook_web_tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
+++ addressbook_web_tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -2,7 +2,6 @@ package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
 /**
  * Created by Daria on 06.06.2016.
@@ -15,12 +14,18 @@ public class NavigationHelper extends HelperBase {
     }
 
     public void gotoGroupPage() {
-
+        if (isElementPresent(By.tagName("h1"))
+                && wd.findElement(By.tagName("h1")).getText().equals("Groups")
+                && isElementPresent(By.name("new"))) {
+            return;
+        }
         click(By.linkText("groups"));
     }
 
     public void goToAddNewPage() {
-
+        if (isElementPresent(By.id("maintable"))) {
+            return;
+        }
         click(By.linkText("add new"));
     }
 }
