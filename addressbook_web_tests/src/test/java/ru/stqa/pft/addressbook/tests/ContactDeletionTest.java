0f914653eb4453f2edda231cfc2cf08abbe8f38d
DariaChurkina <d_churkina@inbox.ru>
2016-06-22 00:39:25 +0300

    Внесены изменения для проверки предусловия выполнения теста: наличие уже созданной группы.

--- addressbook_web_tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTest.java
+++ addressbook_web_tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTest.java
@@ -1,12 +1,18 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.ContactData;
 
 public class ContactDeletionTest extends TestBase {
 
     @Test
     public void testContactDeletion() {
         app.goToHomePage();
+        if (! app.getContactHelper().isThereAContact()) {
+            app.getNavigationHelper().goToAddNewPage();
+            app.getContactHelper().createContact(new ContactData("Daria", "Churkina", "123", "daria.churkina@inbox.ru", "Test1"));
+            app.goToHomePage();
+        }
         app.getContactHelper().selectContact();
         app.getContactHelper().deleteSelectedContact();
         app.getAccept(); //Как бороться с незаписывающимися действиями для диалогового окна
