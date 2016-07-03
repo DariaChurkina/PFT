2d2b211592a43021833c6554553b712978970bc4
DariaChurkina <d_churkina@inbox.ru>
2016-07-04 00:10:30 +0300

    Начало изменений по l4_m5

--- addressbook_web_tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTest.java
+++ addressbook_web_tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTest.java
@@ -4,18 +4,20 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 
+import java.util.List;
+
 public class ContactCreationTest extends TestBase {
     
     @Test
     public void testContactCreation() {
 
         app.goToHomePage();
-        int before = app.getContactHelper().getContactCount();
+        List<ContactData> before = app.getContactHelper().getContactList();
         app.getNavigationHelper().goToAddNewPage();
         app.getContactHelper().fillContactData(new ContactData("Daria", "Churkina", "123", "daria.churkina@inbox.ru", "Test1"), true);
         app.getContactHelper().submitNewContact();
         app.goToHomePage();
-        int after = app.getContactHelper().getContactCount();
-        Assert.assertEquals(after, before +1);
+        List<ContactData> after = app.getContactHelper().getContactList();
+        Assert.assertEquals(after.size(), before.size() +1);
     }
 }
