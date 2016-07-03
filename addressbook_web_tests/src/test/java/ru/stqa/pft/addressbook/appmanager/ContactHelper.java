97729037b4511451b38f22f0534d7fd8954b17af
DariaChurkina <d_churkina@inbox.ru>
2016-07-04 00:10:51 +0300

    Начало изменений по l4_m5

--- addressbook_web_tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook_web_tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -2,10 +2,14 @@ package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import ru.stqa.pft.addressbook.model.ContactData;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Daria on 06.06.2016.
@@ -67,5 +71,15 @@ public class ContactHelper extends HelperBase {
 
     public int getContactCount() {
        return wd.findElements(By.name("selected[]")).size();
+    }
+
+    public List<ContactData> getContactList() {
+        List<ContactData> contacts = new ArrayList<ContactData>();
+        List<WebElement> elements = wd.findElements(By.cssSelector("class.title"));
+        for (WebElement element : elements) {
+            String titleName = element.getText();
+            ContactData contact = new ContactData(titleName);
+        }
+        return contacts;
     }
 }
