62d208325a0f374a706d38ba16fe449cad271364
DariaChurkina <d_churkina@inbox.ru>
2016-07-16 16:57:34 +0300

    Реализация fluent проверок. GroupCreationTest

--- addressbook_web_tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTest.java
+++ addressbook_web_tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTest.java
@@ -19,7 +19,6 @@ public class GroupCreationTest extends TestBase {
         Groups after = app.group().all();
         assertThat(after, equalTo(
                 before.withAdded( group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
-
     }
 
     @Test
