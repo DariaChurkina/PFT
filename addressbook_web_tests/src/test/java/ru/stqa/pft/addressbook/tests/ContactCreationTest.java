87b1f540883b361d1960db68779822ff3a90b98d
DariaChurkina <d_churkina@inbox.ru>
2016-07-28 23:54:34 +0300

    ContactCreationTest, L6_m5, падает.

--- addressbook_web_tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTest.java
+++ addressbook_web_tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTest.java
@@ -1,25 +1,39 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactCreationTest extends TestBase {
 
-    @Test
-    public void testContactCreation() {
+    @DataProvider
+    public Iterator<Object[]> validContacts() {
+        List<Object[]> list = new ArrayList<Object[]>();
+        File photo = new File("src/test/resources/helena.jpg");
+        list.add(new Object[] {new ContactData().withFirstname("Daria").withLastname("Churkina")
+                .withPhone("123").withEmail("daria@gmail.com").withGroup("Test1").withPhoto(photo)});
+        list.add(new Object[] {new ContactData().withFirstname("Maria").withLastname("Turkina")
+                .withPhone("456").withEmail("maria@gmail.com").withGroup("Test1").withPhoto(photo)});
+        list.add(new Object[] {new ContactData().withFirstname("Antonina").withLastname("Chekhova")
+                .withPhone("789").withEmail("antonina@gmail.com").withGroup("Test1").withPhoto(photo)});
+        return list.iterator();
+    }
 
+
+    @Test(dataProvider = "validContacts")
+    public void testContactCreation(ContactData contact) {
         app.goToHomePage();
         Contacts before = app.contact().all();
-        File photo = new File("src/test/resources/helena.jpg");
         app.goTo().addNewPage();
-        ContactData contact = new ContactData().withFirstname("Daria")
-                .withLastname("Churkina").withPhone("123").withEmail("daria.churkina@inbox.ru").withGroup("Test1").withPhoto(photo);
         app.contact().submit(contact);
         app.contact().goToHomePage();
         assertThat(app.contact().count(), equalTo(before.size() + 1));
@@ -27,6 +41,7 @@ public class ContactCreationTest extends TestBase {
         assertThat(after, equalTo
                 (before.withAdded(contact.withId(after.stream().mapToInt((c) -> c.getId()).max().getAsInt()))));
     }
+
 
     /*@Test (тест на проверку абсолютного адреса фото)
     public void testCurrentdir() {
